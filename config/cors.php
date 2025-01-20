6b463c6c232faf763743eb8f5a5581b086e3e920
CharlesCasenas <casenascharles@gmail.com>
2025-01-20 22:32:40 +0800

    Changed the cors to allow the deployed frontend from vercel

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://portfolio-kasenyashi.vercel.app/'],
+    'allowed_origins' => ['https://portfolio-kasenyashi.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
