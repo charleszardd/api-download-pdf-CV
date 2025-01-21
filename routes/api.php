27be8db0b9091374ea36b9b3dbd368a091038d47
CharlesCasenas <casenascharles@gmail.com>
2025-01-21 10:01:57 +0800

    Revert back to the prev api code

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DownloadPDFController;
 
-Route::middleware(['cors'])->group(function () {
-    Route::get('pdf-download-api', [DownloadPDFController::class, 'downloadCV']);
-});
+Route::get('pdf-download-api', [DownloadPDFController::class, 'downloadCV']);
 
 
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
